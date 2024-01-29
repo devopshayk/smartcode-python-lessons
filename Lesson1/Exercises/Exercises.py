5fff35407b402f88be5f167d7b063656103fb82d
devopshayk <devopshayk@gmail.com>
2024-01-29 14:50:17 +0400

    lesson 1

--- Lesson1/Exercises/Exercises.py
+++ Lesson1/Exercises/Exercises.py
@@ -35,6 +35,19 @@
 
 
 
+# side1 = float(input("Enter length of first side: "))
+# side2 = float(input("Enter length of second side: "))
+
+# two_angles_less_than_90 = int(side1 < 90) + int(side2 < 90) < 2
+# one_side_not_zero = side1 * side2 != 0
+# has_right_angle = (side1 * side1 + side2 * side2 == ((side1 < 90) + (side2 < 90)) * (side1 < 90) * side1 * side1 + ((side1 < 90) + (side2 < 90)) * (side2 < 90) * side2 * side2)
+
+# print("The triangle has a right angle." * has_right_angle + "The triangle does not have a right angle." * (not has_right_angle) * (two_angles_less_than_90 * one_side_not_zero * has_right_angle))
+
+# --------------> task 5
+
+
+
 # first_angle = float(input('Write first angle: '))
 # second_angle = float(input('Write second angle: '))
 # third_angle = int(180 - (first_angle + second_angle))
@@ -46,8 +59,38 @@
 
 
 # number = float(input("Enter a number: "))
+# is_greater_than_100 = number > 100
+
+# print(is_greater_than_100)
+
+# # --------------> task 7
+
+
+
+# number = float(input("Enter a number: "))
 # divisibleNum = number % 400 == 0
 
 # print(divisibleNum)
 
 # --------------> task 8
+
+
+
+# radius = float(input("Enter the radius of the wheel in meters: "))
+
+# circumference = 2 * 3.14 * radius  
+# total_distance = 5 * circumference
+
+# print("The wheel will pass", total_distance, "meters after 5 turns.")
+
+# --------------> task 9
+
+
+
+# Exercise 1: Even or Odd Number
+# Write a Python program that asks the user to input a number and then determines whether the number is even or odd. The program should print "Even" if the number is even and "Odd" if the number is odd.
+
+# Exercise 2: Sum of Two Numbers
+# Write a Python program that asks the user to input two numbers and then calculates and prints their sum.
+
+# --------------> task 10
