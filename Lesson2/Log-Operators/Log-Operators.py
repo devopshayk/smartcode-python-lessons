0c2428ea0e866121352abe33074e5f8b86ae9f24
devopshayk <devopshayk@gmail.com>
2024-01-31 15:35:22 +0400

    lesson 2

--- Lesson2/Log-Operators/Log-Operators.py
+++ Lesson2/Log-Operators/Log-Operators.py
@@ -9,3 +9,53 @@
 
 
 
+# fruit1 = input('Write your fruite: ')
+# fruit2 = input('Write your fruite: ')
+# fruit3 = input('Write your fruite: ')
+
+# result = 'orange' in fruit1 and 'banana' in fruit2 and 'fridge' in fruit3
+
+# print(result)
+
+# --------------> task 2
+
+
+
+# x = float(input("Enter a number: "))
+
+# condition1 = x <= -8 or x > 12
+# condition2 = x > 10 and x <= 50
+# condition3 = x > -10 and x < 10
+# condition4 = x != 20 or x > 50
+
+# print("Condition 1:", condition1)
+# print("Condition 2:", condition2)
+# print("Condition 3:", condition3)
+# print("Condition 4:", condition4)
+
+# --------------> task 3
+
+
+
+# fridge = "bananas oranges apples"
+
+# have_bananas = "bananas" in fridge
+# have_oranges = "oranges" in fridge
+# have_apples = "apples" in fridge
+
+# home = "light"
+
+# have_light = "light" in home
+
+# buy_message = (not have_bananas) * "You don't have bananas. Please buy some. " + \
+#               (not have_oranges) * "You don't have oranges. Please buy some. " + \
+#               (not have_apples) * "You don't have apples. Please buy some."
+
+# take_items_message = (not have_light) * "You don't have a light at home. Taking out items from the fridge. Now your fridge is empty."
+
+# print(buy_message)
+# print(take_items_message)
+
+
+
+# --------------> task 4
