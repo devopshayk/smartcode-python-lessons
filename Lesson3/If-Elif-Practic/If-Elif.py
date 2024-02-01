71a033c4522079f8abcdc996766231536d4a8351
devopshayk <devopshayk@gmail.com>
2024-02-01 16:43:52 +0400

    lesson 3

---
+++ Lesson3/If-Elif-Practic/If-Elif.py
@@ -0,0 +1,13 @@
+# raining = float(input('Is it raining? '))
+
+# if raining == 1:
+#     print('Yes raining!')
+# elif raining == 0:
+#     print('No raining!')
+# else:
+#     print('Error:404')    
+    
+# --------------> task 1
+
+
+
