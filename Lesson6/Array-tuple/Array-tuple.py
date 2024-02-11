297a81d52726be1ffe7ca4c17c9d88822574d250
devopshayk <devopshayk@gmail.com>
2024-02-11 04:17:04 +0400

    lesson 6

---
+++ Lesson6/Array-tuple/Array-tuple.py
@@ -0,0 +1,32 @@
+# arr = [5, 5, 1, 65, 48, 75]
+
+# sum = 0
+# for i in arr:
+#     sum += i
+# print(sum)    
+
+# --------------> task 1
+
+
+
+# arr = [5, 5, 1, 65, 48, 75]
+
+# sum = 1
+# for i in arr:
+#     sum *= i
+# print(sum)  
+
+# --------------> task 2
+
+
+
+# text_list = ["This is a short text.", "This is a longer text.", "This is the longest text among all."]
+
+# largest_text = text_list[0]
+# for text in text_list:
+#     if len(text) > len(largest_text):
+#         largest_text = text
+
+# print("The largest text is:", largest_text)
+
+# --------------> task 3
