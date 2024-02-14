5b1531c1dbbc1c58ab9fb4679454277e8f0e0f3b
devopshayk <devopshayk@gmail.com>
2024-02-14 16:11:20 +0400

    lesson 7

---
+++ Lesson7/Dictionaries/Dictionaries.py
@@ -0,0 +1,73 @@
+# my_dict = {'apple': 4, 'banana': 2, 'orange': 8, 'grape': 6}
+
+# sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1]))
+
+# print("Sorted dictionary by value:")
+# for key, value in sorted_dict.items():
+#     print(key, ":", value)
+
+# --------------> task 1
+
+
+
+# my_dict = {'apple': 4, 'banana': 2, 'orange': 8, 'grape': 6}
+
+# my_dict['kiwi'] = 5
+
+# print("Updated dictionary after adding a new key:")
+# print(my_dict)
+
+# --------------> task 2
+
+
+
+# my_dict = {'apple': 4, 'banana': 2, 'orange': 8, 'grape': 6}
+
+# key_to_check = 'orange'
+
+# if key_to_check in my_dict:
+#     print(f"The key '{key_to_check}' exists in the dictionary.")
+# else:
+#     print(f"The key '{key_to_check}' does not exist in the dictionary.")
+
+
+
+# --------------> task 3
+
+
+
+# dict1 = {'a': 50, 'b': 700}
+# dict2 = {'c': 400, 'd': 600}
+
+# merged_dict = dict1.copy()
+# merged_dict.update(dict2)
+
+# print("Merged dictionary:")
+# print(merged_dict)
+
+# --------------> task 4
+
+
+
+# my_dict = {'a': 1, 'b': 2, 'c': 12}
+
+# product = 1
+
+# for value in my_dict.values():
+#     product *= value
+
+# print("Product of all values in the dictionary:", product)
+
+# --------------> task 5
+
+
+
+# my_dict = {'D': 56, 'E': 12, 'F': 69, 'C': 45, 'B': 23, 'A': 67}
+
+# sorted_dict = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
+
+# highest_3_values = dict(sorted_dict[:3])
+
+# print("Highest 3 values in the dictionary:", highest_3_values)
+
+# --------------> task 6
---
+++ Lesson7/Dictionaries2/Dictionaries2.py
@@ -0,0 +1,43 @@
+# students_ratings = {
+#     'Student1': 8,
+#     'Student2': 7,
+#     'Student3': 9,
+#     'Student4': 6,
+#     'Student5': 9,
+#     'Student6': 8,
+#     'Student7': 7,
+#     'Student8': 6,
+#     'Student9': 9,
+#     'Student10': 8
+# }
+
+# print("Students and their ratings:")
+# for student, rating in students_ratings.items():
+#     print(f"{student}: {rating}")
+
+# --------------> task 1
+
+
+
+# students_ratings = {
+#     'Student1': 8,
+#     'Student2': 7,
+#     'Student3': 9,
+#     'Student4': 6,
+#     'Student5': 9,
+#     'Student6': 8,
+#     'Student7': 7,
+#     'Student8': 6,
+#     'Student9': 9,
+#     'Student10': 8
+# }
+
+# total_sum = sum(students_ratings.values())
+
+# total_students = len(students_ratings)
+
+# average_rating = total_sum / total_students
+
+# print("Arithmetic average of ratings of students:", average_rating)
+
+# --------------> task 2
